92ede27d6517b3972a572569faf468517c45af9b
njmyers <17010801+njmyers@users.noreply.github.com>
2021-07-04 17:24:37 -0400

    Add order repository

---
+++ src/domains/billing/repositories/order.ts
@@ -0,0 +1,81 @@
+import { Populate } from '@mikro-orm/core';
+import { orm } from '../../../lib/context';
+import { ApplicationError, StatusCode, ErrorCode } from '../../../lib/error';
+import { Order, OrderFields, LineItemFields, LineItem } from '../models';
+import { User } from '../../shop/models';
+
+export interface OrderInput extends OrderFields {
+  lineItems: LineItemFields[];
+  customer: User;
+}
+
+async function findById(
+  id: number,
+  populate?: Populate<Order>
+): Promise<Order | null> {
+  return await orm.em.findOne(Order, { id }, populate);
+}
+
+async function findByNumber(
+  number: string,
+  populate?: Populate<Order>
+): Promise<Order | null> {
+  return await orm.em.findOne(Order, { number }, populate);
+}
+
+function create(input: OrderInput): Order {
+  const { lineItems, customer, ...fields } = input;
+  const order = new Order(fields);
+
+  if (Array.isArray(lineItems)) {
+    lineItems.forEach(lineItem => {
+      order.lineItems.add(new LineItem(lineItem));
+    });
+  }
+
+  if (customer) {
+    order.customer = customer;
+  }
+
+  orm.em.persist(order);
+
+  return order;
+}
+
+async function update(id: number, input: Partial<OrderInput>): Promise<Order> {
+  const order = await orm.em.findOne(Order, id);
+
+  if (!order) {
+    throw new ApplicationError('Cannot find order with id', {
+      status: StatusCode.NotFound,
+      code: ErrorCode.ERROR_NOT_FOUND,
+      data: {
+        id,
+      },
+    });
+  }
+
+  const { lineItems, ...fields } = input;
+
+  if (Array.isArray(lineItems)) {
+    lineItems.forEach(lineItem => {
+      order.lineItems.add(new LineItem(lineItem));
+    });
+  }
+
+  Object.entries(fields).forEach(([field, value]) => {
+    if (value) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      order[field] = value;
+    }
+  });
+
+  return order;
+}
+
+export const orderRepo = {
+  findById,
+  findByNumber,
+  create,
+  update,
+};
